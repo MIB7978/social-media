d7e92d2aaa97656e5b22bfbce84737167cd428d4
suraj <sahusuraj7978@gmail.com>
2023-02-26 18:59:43 +0530

    frontend

---
+++ src/components/post/Post.jsx
@@ -0,0 +1,51 @@
+import "./post.scss"
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import FavoriteOutlinedIcon from "@mui/icons-material/FavoriteOutlined";
+import TextsmsOutlinedIcon from "@mui/icons-material/TextsmsOutlined";
+import ShareOutlinedIcon from "@mui/icons-material/ShareOutlined";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import { Link } from "react-router-dom";
+import { Comments } from "../comments/Comments";
+import { useState } from "react";
+export const Post = ({ post }) => {
+    const liked = true;
+    const [commentOpen, setCommentOpen] = useState(false)
+    return (
+        <div className="post">
+            <div className="container">
+                <div className="user">
+                    <div className="userInfo">
+                        <img src={post.profilePic} alt="" />
+                        <div className="details" >
+                            <Link to={`/profile/${post.userId}`} style={{ textDecoration: 'none', color: "inherit" }}>
+                                <span className="name">{post.name}</span>
+                            </Link>
+                            <span className="date">1 min ago </span>
+
+                        </div>
+                    </div>
+                    <MoreHorizIcon />
+                </div>
+                <div className="content">
+                    <p>{post.desc}</p>
+                    <img src={post.img} alt="" />
+                </div>
+                <div className="info">
+                    <div className="item">
+                        {!liked ? <FavoriteBorderOutlinedIcon /> : <FavoriteOutlinedIcon />}
+                        12 likes
+                    </div>
+                    <div className="item" onClick={() => setCommentOpen(!commentOpen)}>
+                        <TextsmsOutlinedIcon />
+                        12  comments
+                    </div>
+                    <div className="item">
+                        <ShareOutlinedIcon />
+                        share
+                    </div>
+                </div>
+                {commentOpen && <Comments />}
+            </div>
+        </div>
+    )
+}
---
+++ src/components/right/Right.jsx
@@ -0,0 +1,125 @@
+import React from 'react'
+import "./right.scss"
+export const Right = () => {
+    return (
+        <div className='rightbar'>
+            <div className="container">
+                <div className="item">
+                    <span>Suggestions for you</span>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+                            <span>John Doe</span>
+                        </div>
+                        <div className="buttons">
+                            <button>Follow</button>
+                            <button>Dismiss</button>
+                        </div>
+                    </div>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+                            <span>John Doe</span>
+                        </div>
+                        <div className="buttons">
+                            <button>Follow</button>
+                            <button>Dismiss</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="item">
+                    <span>Latest Activities</span>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+                            <p>
+                                <span>John Doe</span> Changes pictute
+
+                            </p>
+                        </div>
+                        <span className='date-span'>
+                            1 min ago
+                        </span>
+                    </div>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+                            <p>
+                                <span>John Doe</span> Changes pictute
+
+                            </p>
+                        </div>
+                        <span className='date-span'>
+                            1 min ago
+                        </span>
+                    </div>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+                            <p>
+                                <span>John Doe</span> Changes pictute
+
+                            </p>
+                        </div>
+                        <span className='date-span'>
+                            1 min ago
+                        </span>
+                    </div>
+                </div>
+                <div className="item">
+                    <span>Other friends</span>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+                            <div className="online" />
+
+                            <span>John Doe</span>
+
+
+                        </div>
+                    </div>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+                            <div className="online" />
+
+                            <span>John Doe</span>
+
+
+                        </div>
+                    </div>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+                            <div className="online" />
+
+                            <span>John Doe</span>
+
+
+                        </div>
+                    </div>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+                            <div className="online" />
+
+                            <span>John Doe</span>
+
+
+                        </div>
+                    </div>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+                            <div className="online" />
+
+                            <span>John Doe</span>
+
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
